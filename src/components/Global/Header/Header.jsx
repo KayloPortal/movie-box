791f2309edd6e2c41d9e63b935e07270789d9315
kayloportal <pinobaxp@gmail.com>
2024-03-27 22:07:50 +0330

    added/ menu modal component static

--- src/components/Global/Header/Header.jsx
+++ src/components/Global/Header/Header.jsx
@@ -1,13 +1,18 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Header.css";
 import iconLogo from "/icons/logo.svg";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import iconMenu from "/icons/menu.svg";
 import iconSearch from "/icons/search.svg";
+import iconMenuClose from "/icons/menu-close.svg";
+import iconArrowLeft from "/icons/arrow-left.svg";
+import gsap from "gsap"
+import useGSAP from "@gsap/react"
 
 function Header({ displaySearch, bgTransparent }) {
   const [query, setQuery] = useState("");
+  const [open, setOpen] = useState(false);
   function handleQuery(newData) {
     setQuery(newData);
   }
@@ -31,20 +36,61 @@ function Header({ displaySearch, bgTransparent }) {
               placeholder="What do you want to watch?"
             />
             <button className="query-form__submit" type="submit">
-              <img className="query-from__submit-icon" src={iconSearch} alt="submit search" />
+              <img
+                className="query-from__submit-icon"
+                src={iconSearch}
+                alt="submit search"
+              />
             </button>
           </form>
         ) : (
           false
         )}
         <div className="hello \ ">
-          <Link className=" | fw-bold" to="/">Dashboard</Link>
+          <Link className=" | fw-bold" to="/">
+            Dashboard
+          </Link>
           <button className="menu-button">
             <img className="menu-button__icon" src={iconMenu} alt="open menu" />
           </button>
         </div>
       </div>
+      <MenuModal />
     </header>
+  );
+}
+
+function MenuModal({ open, toggleOpen }) {
+
+  return (
+    <div className="menu-backdrop" onClick={() => toggleOpen(false)}>
+      <div className="menu">
+        <div className="container menu-container">
+          <button className="menu-button" onClick={() => toggleOpen(false)}>
+            <img className="menu-button__icon" src={iconMenuClose} alt="close menu" />
+          </button>
+          <nav className="menu-navi">
+            <ul className="menu-navi-list" role="list">
+              <li className="menu-navi-list__item">
+                <NavLink to="/" className="menu-navi__link | fs-600 fl-height-600 link-underline">
+                  Home <img src={iconArrowLeft} alt="You're at Home page" />
+                </NavLink>
+              </li>
+              <li className="menu-navi-list__item">
+                <NavLink to="/movies" className="menu-navi__link | fs-600 fl-height-600 link-underline">
+                  Movies <img src={iconArrowLeft} alt="You're at Movies page" />
+                </NavLink>
+              </li>
+              <li className="menu-navi-list__item">
+                <NavLink to="/dashboard" className="menu-navi__link | fs-600 fl-height-600 link-underline">
+                  Dashboard <img src={iconArrowLeft} alt="You're at Dashboard page" />
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
   );
 }
 
