4cdcf261ff5af9346ed56211c1942d0d699f72ba
kayloportal <pinobaxp@gmail.com>
2024-03-27 21:09:23 +0330

    added/ Header component's base styles, static version

--- src/components/Global/Header/Header.jsx
+++ src/components/Global/Header/Header.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import "./Header.css";
-import imageLogo from "/images/logo.png";
+import iconLogo from "/icons/logo.svg";
 import { Link } from "react-router-dom";
 import iconMenu from "/icons/menu.svg";
 import iconSearch from "/icons/search.svg";
@@ -17,28 +17,28 @@ function Header({ displaySearch, bgTransparent }) {
       style={{
         backgroundColor: bgTransparent ? "var(--gray-900)" : "var(--gray-900)",
       }}
-      className="header | neutral-100"
+      className="header | neutral-100 fs-300 fl-height-600 "
     >
       <div className="container header-container">
-        <img src={imageLogo} alt="MovieBox" className="header__logo" />
+        <img src={iconLogo} alt="MovieBox" className="header__logo" />
         {displaySearch ? (
-          <form action="" className="header-query">
+          <form action="" className="query-form">
             <input
               type="text"
               value={query}
               onChange={({ target }) => handleQuery(target.value)}
-              className="header-query__input"
-              placeholder="Search"
+              className="query-form__input"
+              placeholder="What do you want to watch?"
             />
-            <button className="header-query__submit" type="submit">
-              <img src={iconSearch} alt="submit search" />
+            <button className="query-form__submit" type="submit">
+              <img className="query-from__submit-icon" src={iconSearch} alt="submit search" />
             </button>
           </form>
         ) : (
           false
         )}
         <div className="hello \ ">
-          <Link to="/" />
+          <Link className=" | fw-bold" to="/">Dashboard</Link>
           <button className="menu-button">
             <img className="menu-button__icon" src={iconMenu} alt="open menu" />
           </button>
