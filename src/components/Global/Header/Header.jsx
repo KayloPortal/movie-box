02ebc7f86f4a7c29ca7d8818e145e55e36a1d7a3
kayloportal <pinobaxp@gmail.com>
2024-03-28 19:50:42 +0330

    added/ header dynamic version with gsap animations

--- src/components/Global/Header/Header.jsx
+++ src/components/Global/Header/Header.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import "./Header.css";
 import iconLogo from "/icons/logo.svg";
 import { Link, NavLink } from "react-router-dom";
@@ -7,15 +7,36 @@ import iconMenu from "/icons/menu.svg";
 import iconSearch from "/icons/search.svg";
 import iconMenuClose from "/icons/menu-close.svg";
 import iconArrowLeft from "/icons/arrow-left.svg";
-import gsap from "gsap"
-import useGSAP from "@gsap/react"
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 function Header({ displaySearch, bgTransparent }) {
   const [query, setQuery] = useState("");
   const [open, setOpen] = useState(false);
+
   function handleQuery(newData) {
     setQuery(newData);
   }
+
+  function toggleOpen(to = undefined) {
+    setOpen((prev) => (to !== undefined ? to : !prev));
+  }
+
+  const header = useRef();
+  useGSAP(
+    () => {
+      console.log("ran");
+      console.log(open)
+      gsap.fromTo(".menu", {x: open ? "0" : "-30rem"}, { x: open ? "-30rem" : "0", duration: .25, display: open? "unset": "none" });
+      gsap.to(".menu-backdrop", {
+        opacity: open ? 1 : 0,
+        zIndex: open ? 2 : -1,
+        // display: open? "unset" : "none",
+        duration: .25
+      });
+    },
+    { scope: header, dependencies: [open] }
+  );
 
   return (
     <header
@@ -23,6 +44,7 @@ function Header({ displaySearch, bgTransparent }) {
         backgroundColor: bgTransparent ? "var(--gray-900)" : "var(--gray-900)",
       }}
       className="header | neutral-100 fs-300 fl-height-600 "
+      ref={header}
     >
       <div className="container header-container">
         <img src={iconLogo} alt="MovieBox" className="header__logo" />
@@ -50,40 +72,54 @@ function Header({ displaySearch, bgTransparent }) {
           <Link className=" | fw-bold" to="/">
             Dashboard
           </Link>
-          <button className="menu-button">
+          <button className="menu-button" onClick={toggleOpen}>
             <img className="menu-button__icon" src={iconMenu} alt="open menu" />
           </button>
         </div>
       </div>
-      <MenuModal />
+      <MenuModal open={open} toggleOpen={toggleOpen} />
     </header>
   );
 }
 
-function MenuModal({ open, toggleOpen }) {
+function MenuModal({ toggleOpen }) {
 
   return (
     <div className="menu-backdrop" onClick={() => toggleOpen(false)}>
       <div className="menu">
         <div className="container menu-container">
           <button className="menu-button" onClick={() => toggleOpen(false)}>
-            <img className="menu-button__icon" src={iconMenuClose} alt="close menu" />
+            <img
+              className="menu-button__icon"
+              src={iconMenuClose}
+              alt="close menu"
+            />
           </button>
           <nav className="menu-navi">
             <ul className="menu-navi-list" role="list">
               <li className="menu-navi-list__item">
-                <NavLink to="/" className="menu-navi__link | fs-600 fl-height-600 link-underline">
+                <NavLink
+                  to="/"
+                  className="menu-navi__link | fs-600 fl-height-600 link-underline"
+                >
                   Home <img src={iconArrowLeft} alt="You're at Home page" />
                 </NavLink>
               </li>
               <li className="menu-navi-list__item">
-                <NavLink to="/movies" className="menu-navi__link | fs-600 fl-height-600 link-underline">
+                <NavLink
+                  to="/movies"
+                  className="menu-navi__link | fs-600 fl-height-600 link-underline"
+                >
                   Movies <img src={iconArrowLeft} alt="You're at Movies page" />
                 </NavLink>
               </li>
               <li className="menu-navi-list__item">
-                <NavLink to="/dashboard" className="menu-navi__link | fs-600 fl-height-600 link-underline">
-                  Dashboard <img src={iconArrowLeft} alt="You're at Dashboard page" />
+                <NavLink
+                  to="/dashboard"
+                  className="menu-navi__link | fs-600 fl-height-600 link-underline"
+                >
+                  Dashboard{" "}
+                  <img src={iconArrowLeft} alt="You're at Dashboard page" />
                 </NavLink>
               </li>
             </ul>
