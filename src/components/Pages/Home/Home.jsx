31c46ec9b30c9cc6296e8028311c3765c2f1f75b
kayloportal <pinobaxp@gmail.com>
2024-03-30 15:05:12 +0330

    Home/Hero section static

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,9 +1,88 @@
-import "./Home.css"
+/* eslint-disable react/prop-types */
+import "./Home.css";
+import iconIMDB from "/icons/imdb.svg";
+import iconTomato from "/icons/tomato.svg";
+import iconPlaySolid from "/icons/play-solid.svg";
+import imageJohnWick3 from "/images/banner-johnwick3.jpg";
+import imageDankirk from "/images/banner-dankirk.jpg";
+import imageFury from "/images/banner-fury.jpg";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+  return <Hero />;
 }
 
-export default Home
+const heroMovies = [
+  {
+    title: "John Wick 3 : Parabellum",
+    description:
+      "John Wick is on the run after killing a member of the international assassins' guild, and with a $14 million price tag on his head, he is the target of hit men and women everywhere.",
+    "image-url": imageJohnWick3,
+    "rate-IMDb": "8.6",
+    "rate-Tomato": "97",
+    id: "yf7834gd8634gd"
+  },
+  {
+    title: "Dankirk",
+    description:
+      "Allied soldiers from Belgium, the British Commonwealth and Empire, and France are surrounded by the German Army and evacuated during a fierce battle in World War II.",
+    "image-url": imageDankirk,
+    "rate-IMDb": "7.8",
+    "rate-Tomato": "94",
+    id: "yf7834gdg73486"
+  },
+  {
+    title: "Fury",
+    description:
+      "A grizzled tank commander makes tough decisions as he and his crew fight their way across Germany in April, 1945.",
+    "image-url": imageFury,
+    "rate-IMDb": "7.6",
+    "rate-Tomato": "76",
+    id: "yf7834gdg734gd"
+  },
+];
+
+function Hero() {
+  return (
+    <section className="hero-section">
+      <h1 className="visually-hidden">Free Movie And Drama Website</h1>
+      <div className="hero-slider">
+        <h2 className="visually-hidden">Most Popular Movies</h2>
+        {heroMovies.map((data) => <HeroSlide key={data.id} movie={data} />)}
+      </div>
+    </section>
+  );
+}
+
+function HeroSlide({movie}) {
+  return (
+    <div
+      className="hero-slide | neutral-100"
+      style={{ background: `url(${movie["image-url"]}) no-repeat center`, backgroundSize: "cover"}}
+    >
+      <div className="hero-slide__bg"></div>
+      <div className="container hero-slide-container">
+        <h3 className="hero-slide__heading | fw-bold fs-700 fl-height-700">
+          {movie.title}
+        </h3>
+        <p className="hero-slide__rates">
+          <img src={iconIMDB} alt="IMDb rate" /> {movie["rate-IMDb"] * 10}.0 / 100{" "}
+          <img
+            src={iconTomato}
+            alt="Rotten-Tomatoes rate"
+            style={{ marginLeft: "2.125rem" }}
+          />{" "}
+          {movie["rate-tomato"]}%
+        </p>
+        <p className="hero-slide__desc | fs-200 fl-height-400 fw-medium">
+          {movie.description}
+        </p>
+        <button className="hero-slide__cta | fs-200 fl-height-600 fw-bold">
+          {" "}
+          <img src={iconPlaySolid} alt="play icon" /> WATCH TRAILER
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
