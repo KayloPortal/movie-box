9424a7265c64ec09a97e3f737c8e58daecc0551b
kayloportal <pinobaxp@gmail.com>
2024-04-10 10:50:19 +0330

    token context hasLoggedIn

--- src/contexts/tokenContext.jsx
+++ src/contexts/tokenContext.jsx
@@ -1,32 +1,39 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const tokenContext = createContext()
+const tokenContext = createContext();
 
-function TokenProvider({children}) {
-  const localToken = localStorage.getItem("token")
-  const [token, setToken] = useState(
-    localToken ? localToken : false
-  )
+function TokenProvider({ children }) {
+  const localToken = localStorage.getItem("token");
+  const [token, setToken] = useState(localToken ? localToken : "");
+
+  const hasLoggedIn = (function () {
+    if (token) {
+      // fetch and check if the token is valid, then define the status.
+      return true;
+    } else {
+      return false;
+    }
+  })();
 
   useEffect(() => {
-    localStorage.setItem("token", token)
-  }, [token]) 
+    localStorage.setItem("token", token);
+  }, [token]);
 
   const handleToken = (newToken) => {
-    setToken(newToken)
-  }
+    setToken(newToken);
+  };
 
   return (
-    <tokenContext.Provider value={[token, handleToken]}>
+    <tokenContext.Provider value={[token, handleToken, hasLoggedIn]}>
       {children}
     </tokenContext.Provider>
-  )
+  );
 }
 
 export function useTokenContext() {
-  const value = useContext(tokenContext)
-  if (!value) throw("Wrong Scope For TokenContext")
-  return value
+  const value = useContext(tokenContext);
+  if (!value) throw "Wrong Scope For TokenContext";
+  return value;
 }
 
-export default TokenProvider
+export default TokenProvider;
